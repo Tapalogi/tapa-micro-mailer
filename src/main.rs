318ba025e6dd01e6aff6bec6c78c3a4dee66087d
Aditya Kresna <aditya.kresna@outlook.co.id>
2021-01-02 20:05:21 +0000

    Init loggers
    Co-authored-by: Henri Wijaya <henriwijaya81@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,14 @@ pub use log::{debug, error, info, log, warn};
 pub use std::io::{Error as IOError, ErrorKind as IOErrorKind, Result as IOResult};
 
 use config::MailerConfig;
+use utils::{get_hostname, init_logger};
 
 #[tokio::main]
 async fn main() -> IOResult<()> {
+    init_logger();
+
     let runtime_config = MailerConfig::load_from_env()?;
+    let service_hostname = get_hostname();
 
     Ok(())
 }
